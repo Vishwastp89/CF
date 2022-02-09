fbe57d5ee202cc6c189b71959c1d909e72b89222
Vishwastp89 <41899929+Vishwastp89@users.noreply.github.com>
2022-02-09 20:39:54 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ if [ -z ${INPUT_APPDIR+x} ]; then
     cd ${INPUT_APPDIR}
 fi
 
-#cf api ${INPUT_API} ${cf_opts}
+cf api ${INPUT_API} ${cf_opts}
 CF_USERNAME=${INPUT_USERNAME} CF_PASSWORD=${INPUT_PASSWORD} cf auth
 cf target -o ${INPUT_ORG} -s ${INPUT_SPACE}
 cf push -f ${INPUT_MANIFEST}
